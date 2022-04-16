48088b87076dc883029bc641ce9895449d7f7fa2
Douglas Gama <doug_g.o@hotmail.com>
2022-04-16 19:05:28 -0300

    Inicio do projeto

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-inicio').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay : true,
+        autoplaySpeed: 2000
+});
