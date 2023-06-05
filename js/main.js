38df5faea04495044014c54dbb5c58613765115a
douglas-g-o <doug_g.o@hotmail.com>
2023-06-04 23:50:02 -0300

    [implemented images in the banner for when in mobile responsive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $('.home-banner').slick({
         dots: true,
         infinite: true,
-        speed: 300,
+        speed: 500,
         slidesToShow: 1,
         adaptiveHeight: true,
         autoplay : true,
